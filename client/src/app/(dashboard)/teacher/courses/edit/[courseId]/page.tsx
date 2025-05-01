45715235b6f2ce56dfca585d6c1a309da5c89afb
Emil-Domagala <emil.domagalaa@gmail.com>
2025-05-01 18:48:55 +0200

    IDK IM TRYING MY BEST AND FIGHTING WITH TS

--- client/src/app/(dashboard)/teacher/courses/edit/[courseId]/page.tsx
+++ client/src/app/(dashboard)/teacher/courses/edit/[courseId]/page.tsx
@@ -1,9 +1,8 @@
-// import { apiServerService } from '@/lib/api/apiServerSide';
 import EditCourseForm from './EditCourseForm';
 
 const EditCoursePage = async ({ params }: { params: { courseId: string } }) => {
   const { courseId } = await params;
 
-  return <EditCourseForm courseId={courseId} />;
+  return <EditCourseForm courseId={courseId as string} />;
 };
 export default EditCoursePage;
