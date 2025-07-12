6467a51185fe8a6729d8938e0be2f7d3e4440e33
Emil-Domagala <emil.domagalaa@gmail.com>
2025-07-12 22:15:09 +0200

    SendTeacherApplication finished

--- client/src/features/dashboard/user/profile/index.tsx
+++ client/src/features/dashboard/user/profile/index.tsx
@@ -1,11 +1,11 @@
-import BecomeTeacherUser from '@/features/dashboard/user/profile/components/BecomeTeacherUser';
+import SendTeacherApplication from '@/features/dashboard/user/profile/components/SendTeacherApplication';
 import EditUserForm from '@/features/dashboard/user/profile/components/EditUserForm';
 
 const EditUser = ({}) => {
   return (
     <>
       <EditUserForm />
-      <BecomeTeacherUser />
+      <SendTeacherApplication />
     </>
   );
 };
